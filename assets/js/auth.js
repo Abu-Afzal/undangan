315e4da764b3a3bf63ba551ce988202b0cc220a5
Abu-Afzal <anthonioanthox@gmail.com>
2026-06-12 22:24:59 +0800

    Create auth.js

---
+++ assets/js/auth.js
@@ -0,0 +1,20 @@
+import { auth } from './firebase-config.js';
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const formLogin = document.getElementById('formLogin');
+
+if (formLogin) {
+    formLogin.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                window.location.href = "index.html";
+            })
+            .catch((error) => {
+                alert("Login gagal: " + error.message);
+            });
+    });
+}
